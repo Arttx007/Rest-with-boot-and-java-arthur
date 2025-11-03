8ba92275b44ad59bcc7df51abe9f106bef4d0657
Arttx007 <s.arthur.ricardo@gmail.com>
2025-11-03 20:58:55 -0300

    Fist Steps in Java with Spring Boot 3.4.0

---
+++ Rest-with-boot-and-java-arthur/src/main/java/Rest_with_boot_and_java_arthur/Startup.java
@@ -0,0 +1,13 @@
+package Rest_with_boot_and_java_arthur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ Rest-with-boot-and-java-arthur/src/main/java/Rest_with_boot_and_java_arthur/model/Greeting.java
@@ -0,0 +1,3 @@
+package Rest_with_boot_and_java_arthur.model;
+
+public record Greeting(long id, String content) {}
